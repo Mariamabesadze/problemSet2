4ed712a4c3fbafe52b518ac4e8fc26afcf50d8a9
vulpecula <mariam.abesadze029@ens.tsu.edu.ge>
2023-04-28 23:04:57 +0400

    first commit

---
+++ problem2.js
@@ -0,0 +1,69 @@
+// problem 1
+let x = 5;
+console.log(x);
+
+x+=2;
+console.log(x);
+
+x -= 6;
+console.log(x);
+
+x *= 30;
+console.log(x);
+
+x /= 2;
+console.log(x);
+
+x &= 5;
+console.log(x);
+
+x **= 3;
+console.log(x);
+
+
+// problem 3.1
+
+let a = 0.1;
+let b = 0.2;
+
+console.log( a + b == 0.3);
+
+// proble.3.2
+let noun = "true";
+console.log(noun == true);
+
+// problem 3.3
+console.log(1+7 != 8);
+
+// problem 3.4
+console.log(10*5 > 9*6);
+
+// problem 3.5
+console.log(17/6 <= 4);
+
+// problem 4
+let firstName = "james";
+let age = 37;
+let price = 35;
+
+if(age<18){
+    console.log(price == 0);
+}
+else if(firstName == "james" && age == 25){
+    console.log(price %= 80);
+}
+else {
+    console.log(price);
+}
+
+
+
+
+
+
+
+
+
+
+
+
